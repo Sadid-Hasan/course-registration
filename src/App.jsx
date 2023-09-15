f6430b84cb8f20d66503e56f5335c533ae3e8e47
Sadid-Hasan <sadid.hasan94@gmail.com>
2023-09-15 11:22:48 +0600

    Fake Data Ceation and Loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './components/Header/Header'
+import Blogs from './components/Blogs/blogs'
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const blogs = () => {
+    const [blogs,setBlogs] = useState([]);
+    useEffect( () =>{
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    },[])
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default blogs;
